9f3f69f25425e80ff56cb7c2cdc40cb633187e3d
manju05 <XuEvvsuMyu4AG8jSBSw9>
2021-11-20 14:08:23 +0530

    added scheduling logic in fampay appConfig

--- fampay/apps.py
+++ fampay/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import os
 
 
 class FampayConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fampay'
+
+    def ready(self):
+        from .task import youtube_api_scheduler
+
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            youtube_api_scheduler()
